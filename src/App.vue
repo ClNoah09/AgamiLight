2a0480cd75a1fa955a581914d20124248bdc28f4
ClNoah09 <noah.cleeremans@ifosup.wavre.be>
2024-02-07 14:30:21 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
 <template>
   <div class="d-flex flex-column h-100">
     <header>
-      fixed-top bg-dark
       <!-- Fixed navbar -->
-      <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+      <nav
+        class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark"
+        style="background: linear-gradient(90deg, #808080 10%, #0077ff 80%)"
+      >
         <div class="container-fluid">
           <a class="navbar-brand" href="#">Agami-Light</a>
           <button
@@ -46,7 +48,49 @@
     </header>
 
     <!-- Begin page content -->
-    <main class="flex-shrink-0 py-5"></main>
+    <main class="flex-shrink-0 py-5 container-fluid">
+      <div id="carouselExample" class="carousel slide nav">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="./assets/electricien-masculin-travaille-dans-standard-cable-raccordement-electrique (1).jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block w-100" alt="..." />
+          </div>
+          <div class="carousel-item">
+            <img src="..." class="d-block" alt="..." />
+          </div>
+        </div>
+        <button
+          class="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button
+          class="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div class="logo">
+        <img src="./assets/LOGO_NEGATIF.PNG" alt="LOGO" />
+      </div>
+      <button type="button" class="btn btn-primary mt-2 mb-0">
+        Me Contacter
+      </button>
+    </main>
 
     <footer class="footer mt-auto py-3 bg-blue">
       <div class="container-fluid">
@@ -61,7 +105,6 @@
               class="img-fluid smaller-image"
             />
           </div>
-
           <div class="col-4 text-end">
             <a href="mentions-legales.html" class="text-dark"
               >Mentions légales</a
