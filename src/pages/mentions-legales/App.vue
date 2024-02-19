9c64da172ca5d09956175efab6a75ec37a397caa
ClNoah09 <noah.cleeremans@ifosup.wavre.be>
2024-02-19 21:26:45 +0100

    WIP

--- src/pages/mentions-legales/App.vue
+++ src/pages/mentions-legales/App.vue
@@ -9,7 +9,7 @@
         style="background: linear-gradient(90deg, #808080 10%, #0077ff 80%)"
       >
         <div class="container-fluid">
-          <a class="navbar-brand" href="../../../index.html">Agami-Light</a>
+          <a class="navbar-brand" href="#">Agami-Light</a>
           <button
             class="navbar-toggler"
             type="button"
@@ -48,7 +48,84 @@
 
     <!-- Begin page content -->
     <main class="flex-shrink-0 py-5 container-fluid">
-      <h2>HELLO WORLD</h2>
+      <div id="carouselExample " class="carousel slide nav">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img
+              src="../../assets/electricien-masculin-travaille-dans-standard-cable-raccordement-electrique (1).jpg"
+              class="d-block w-100"
+              alt="..."
+            />
+          </div>
+        </div>
+      </div>
+      <div class="Politique de confidentialité">
+        <h3>Politique de confidentialité</h3>
+        <p>
+          Les données stockées dans ma boîte mail sont protégées par un mot de
+          passe et les informations envoyées sont protégées par un encryptage
+          SSL. Vos informations seront utilisées uniquement pour répondre à
+          votre demande. « NathalieVanassche.be » ne stocke aucune information
+          vous concernant dans une base de données.
+        </p>
+      </div>
+      <div class="Droit d'auteur">
+        <h3>Droit d'auteur</h3>
+        <p>
+          Le site « NathalieVanassche.be » constitue une création protégée par
+          le droit d'auteur. Les textes, exercices, correctifs, photos, et
+          autres éléments de ce site sont protégés par le droit d'auteur. Toute
+          copie, adaptation, traduction, arrangement, communication au public,
+          location et autre exploitation, modification de tout ou partie de ce
+          site sous quelle que forme et par quel que moyen que ce soit,
+          électronique, mécanique ou autre, réalisée dans un but lucratif, est
+          strictement interdit sans mon autorisation préalable. Toute infraction
+          à ces droits peut entraîner des poursuites civiles ou pénales.
+          Cependant, ce site est destiné à mes étudiants et le contenu mis en
+          ligne est destiné à être exploité à volonté dans un cadre privé.
+          Ainsi, les copies, impressions, enregistrements sont autorisées sous
+          les seules réserves de ne pas dissimuler les signes distinctifs de
+          personnalisation et de ne pas en faire un usage commercial. En
+          revanche, je n'assure aucune aide, ni support sur des questions
+          d'utilisation ou de compréhension de tous supports disponibles sur ce
+          site en dehors d'un contexte de cours.
+        </p>
+      </div>
+      <div class="Marques et noms commerciaux">
+        <h3>Marques et noms commerciaux</h3>
+        <p>
+          Les dénominations, logos et autres signes utilisés sur ce site sont
+          des marques et/ou noms commerciaux légalement protégés. Tout usage de
+          ceux-ci ou de signes ressemblants est strictement interdit sans un
+          accord préalable et écrit.
+        </p>
+      </div>
+      <div class="Responsabilité quant au contenu">
+        <h3>Responsabilité quant au contenu</h3>
+        <p>
+          J'apporte le plus grand soin à la création et à la mise à jour de ce
+          site mais je ne peux toutefois pas garantir l'exactitude de
+          l'information qui s'y trouve. Les informations contenues dans ce site
+          pourront faire l'objet de modifications sans préavis. Les informations
+          données sur ce site ne sauraient engager ma responsabilité, je ne
+          pourrais être tenue pour responsable de toute omission, erreur ou
+          lacune qui aurait pu se glisser dans ses pages ainsi que des
+          conséquences, quelle qu'elles soient, pouvant résulter de
+          l'utilisation des informations et indications fournies.
+        </p>
+      </div>
+      <div class="Coordonnées">
+        <h3>Coordonnées</h3>
+        <p>
+          Vous pouvez me contacter par Email à
+          nathalie.vanassche@ifosup.wavre.be à l'IFOSUP : www.ifosupwavre.be Rue
+          de la Limite, 6 1300 Wavre 010/22.20.26
+        </p>
+      </div>
+      <div class="Hébergeur">
+        <h3>Hébergeur</h3>
+        <p>OVH 2 rue Kellermann 59100 Roubaix - France. www.ovh.com</p>
+      </div>
     </main>
 
     <footer class="footer mt-auto py-3 bg-blue">
